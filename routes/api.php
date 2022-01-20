ec141a8906427538e45dce48367898a36d46ed21
Bruno Costa <bruno.costa@superlogica.com>
2022-01-20 08:48:27 -0300

    criando o controller

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::resource('cost', CostController::class);
 Route::resource('income', IncomeController::class);
+Route::resource('payment', IncomeController::class)->only(['index', 'create', 'store']);
