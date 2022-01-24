536134b06baf0a5357fe099a752f70efb74c4da3
Bruno Costa <bruno.costa@superlogica.com>
2022-01-24 16:47:58 -0300

    finish

--- resources/views/cost/index.blade.php
+++ resources/views/cost/index.blade.php
@@ -5,10 +5,17 @@
     <div class='card-header'>
         <div class='float-left'><h3 class='m-0'>{{ __('Report of cost')}}</h3></div>
         <div class='float-right'>
-            <span class='text-muted'>{{__('New')}}</span>
-            <a href="{{ route('cost.create.normal') }}" class='btn btn-outline-secondary btn-sm'>{{ __('Cost') }}</a>
-            <a href="{{ route('cost.create.recursive') }}" class='btn btn-outline-info btn-sm'>{{ __('Recursive') }}</a>
-            <a href="{{ route('cost.create.parcel') }}" class='btn btn-outline-warning btn-sm'>{{ __('Parcel') }}</a>
+            <div class="btn-group" role="group">
+                <button id="btnGroupDrop1" type="button" class="btn btn-outline-secondary btn-sm dropdown-toggle" data-toggle="dropdown"
+                    aria-expanded="false">
+                    {{ __('New cost') }}
+                </button>
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="btnGroupDrop1">
+                    <a href="{{ route('cost.create.normal') }}" class="dropdown-item">{{ __('Cost') }}</a>
+                    <a href="{{ route('cost.create.recursive') }}" class="dropdown-item">{{ __('Recursive') }}</a>
+                    <a href="{{ route('cost.create.parcel') }}" class="dropdown-item">{{ __('Parcel') }}</a>
+                </div>
+            </div>
         </div>
     </div>
     @include('includes.charge.filter')
--- resources/views/income/index.blade.php
+++ resources/views/income/index.blade.php
@@ -5,10 +5,17 @@
     <div class='card-header'>
         <div class='float-left'><h3 class='m-0'>{{ __('Report of income')}}</h3></div>
         <div class='float-right'>
-            <span class='text-muted'>{{__('New')}}</span>
-            <a href="{{ route('income.create.normal') }}" class='btn btn-outline-secondary btn-sm'>{{ __('Income') }}</a>
-            <a href="{{ route('income.create.recursive') }}" class='btn btn-outline-info btn-sm'>{{ __('Recursive') }}</a>
-            <a href="{{ route('income.create.parcel') }}" class='btn btn-outline-warning btn-sm'>{{ __('Parcel') }}</a>
+            <div class="btn-group" role="group">
+                <button id="btnGroupDrop1" type="button" class="btn btn-outline-secondary btn-sm dropdown-toggle" data-toggle="dropdown"
+                    aria-expanded="false">
+                    {{ __('New income') }}
+                </button>
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="btnGroupDrop1">
+                    <a href="{{ route('income.create.normal') }}" class="dropdown-item">{{ __('Income') }}</a>
+                    <a href="{{ route('income.create.recursive') }}" class="dropdown-item">{{ __('Recursive') }}</a>
+                    <a href="{{ route('income.create.parcel') }}" class="dropdown-item">{{ __('Parcel') }}</a>
+                </div>
+            </div>
         </div>
     </div>
     @include('includes.charge.filter')
