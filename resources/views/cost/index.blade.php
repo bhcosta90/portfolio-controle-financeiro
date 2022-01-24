28e1dcef4cd831f906cfa82b7c89d36d5881d55e
Bruno Costa <bruno.costa@superlogica.com>
2022-01-24 11:08:54 -0300

    finish

--- resources/views/cost/index.blade.php
+++ resources/views/cost/index.blade.php
@@ -1,5 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-{!! $data->render() !!}
+<div class='card'>
+    <div class='card-header'>
+        <div class='float-left'><h3>{{ __('Report of cost')}}</h3></div>
+        <div class='float-right'>
+            <a href="{{ route('cost.create.normal') }}" class='btn btn-outline-secondary btn-sm'>{{ __('Cost') }}</a>
+            <a href="{{ route('cost.create.recursive') }}" class='btn btn-outline-info btn-sm'>{{ __('Recursive') }}</a>
+            <a href="{{ route('cost.create.parcel') }}" class='btn btn-outline-warning btn-sm'>{{ __('Parcel') }}</a>
+        </div>
+    </div>
+    {!! $data->render() !!}
+</div>
 @endsection
