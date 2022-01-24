b3ea208c0e5104af446435e19cfbf3107f81090f
Bruno Costa <bruno.costa@superlogica.com>
2022-01-24 16:41:32 -0300

    colocando um link

--- resources/views/cost/index.blade.php
+++ resources/views/cost/index.blade.php
@@ -5,6 +5,7 @@
     <div class='card-header'>
         <div class='float-left'><h3 class='m-0'>{{ __('Report of cost')}}</h3></div>
         <div class='float-right'>
+            <span class='text-muted'>{{__('New')}}</span>
             <a href="{{ route('cost.create.normal') }}" class='btn btn-outline-secondary btn-sm'>{{ __('Cost') }}</a>
             <a href="{{ route('cost.create.recursive') }}" class='btn btn-outline-info btn-sm'>{{ __('Recursive') }}</a>
             <a href="{{ route('cost.create.parcel') }}" class='btn btn-outline-warning btn-sm'>{{ __('Parcel') }}</a>
--- resources/views/income/index.blade.php
+++ resources/views/income/index.blade.php
@@ -5,6 +5,7 @@
     <div class='card-header'>
         <div class='float-left'><h3 class='m-0'>{{ __('Report of income')}}</h3></div>
         <div class='float-right'>
+            <span class='text-muted'>{{__('New')}}</span>
             <a href="{{ route('income.create.normal') }}" class='btn btn-outline-secondary btn-sm'>{{ __('Income') }}</a>
             <a href="{{ route('income.create.recursive') }}" class='btn btn-outline-info btn-sm'>{{ __('Recursive') }}</a>
             <a href="{{ route('income.create.parcel') }}" class='btn btn-outline-warning btn-sm'>{{ __('Parcel') }}</a>
