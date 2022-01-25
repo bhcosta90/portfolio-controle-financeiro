78216b754b07338d69dd04a1932b336b38fa631a
Bruno Costa <bruno.costa@superlogica.com>
2022-01-25 09:13:43 -0300

    trocando titulo

--- resources/views/charge/pay.blade.php
+++ resources/views/charge/pay.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class='card'>
         <div class='card-header'>
-            <h3 class='m-0'>{{ __('Cost register')}}</h3>
+            <h3 class='m-0'>{{ __('Payment charge')}}</h3>
         </div>
         <div class='card-body'>
             <div class='form-group'>
