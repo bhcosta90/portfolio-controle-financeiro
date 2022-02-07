b5328e3ba84dbce2447d7ecf9a16ef2a75e6306d
Bruno Costa <bruno.costa@superlogica.com>
2022-02-07 14:10:28 -0300

    finish

--- app/Services/ChargeService.php
+++ app/Services/ChargeService.php
@@ -41,9 +41,10 @@ class ChargeService
         $ret = $this->repository->update($data, $id);
 
         if (!empty($data['update_value'])) {
-            $this->repository->where('chargeable_type', $ret->chargeable_type)
-                ->where('chargeable_id', $ret->chargeable_id)
+            $this->repository->where('basecharge_type', $ret->chargeable_type)
+                ->where('basecharge_id', $ret->chargeable_id)
                 ->where('future', 1)
+                ->where('status', Charge::STATUS_PENDING)
                 ->update([
                     'value_recursive' => $data['value'],
                     'value' => $data['value'],
