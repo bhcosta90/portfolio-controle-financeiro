56309377d696d48587aeed5f6cc7b36f285345b7
Bruno Costa <bruno.costa@superlogica.com>
2022-02-11 13:33:32 -0300

    deletando

--- app/FormPayment/FormPaymentContract.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\FormPayment;
-
-interface FormPaymentContract
-{
-    public function syncFormPayment(array $data);
-}
--- tests/Stub/Traits/ChargeTraitStub.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Tests\Stub\Traits;
-
-use App\Traits\ChargeTrait;
-
-class ChargeTraitStub {
-    use ChargeTrait;
-}
