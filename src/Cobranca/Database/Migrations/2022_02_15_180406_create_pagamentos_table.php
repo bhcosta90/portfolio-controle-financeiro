8fd9dfc815d5efa86c4e4a3db7defd4921271717
Bruno Costa <bruno.costa@superlogica.com>
2022-03-08 12:21:39 -0300

    colocando nulo no pagamento

--- src/Cobranca/Database/Migrations/2022_02_15_180406_create_pagamentos_table.php
+++ src/Cobranca/Database/Migrations/2022_02_15_180406_create_pagamentos_table.php
@@ -20,7 +20,7 @@ class CreatePagamentosTable extends Migration
             $table->string('tenant_id')->index();
             $table->foreignId('user_id')->constrained('users');
             $table->foreignId('cobranca_id')->nullable()->constrained('cobrancas');
-            $table->foreignId('conta_bancaria_id')->constrained('conta_bancarias');
+            $table->foreignId('conta_bancaria_id')->nullable()->constrained('conta_bancarias');
             $table->foreignId('forma_pagamento_id')->constrained('forma_pagamentos');
             $table->foreignId('entidade_id')->nullable()->constrained('entidades');
             $table->string('pagamento_type');
