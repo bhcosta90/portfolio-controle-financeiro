2f0b18fdaac1e67f9755fa0af7406dd0a4a85051
Bruno Costa <bruno.costa@superlogica.com>
2022-06-11 08:22:41 -0300

    config adminlte

--- config/adminlte.php
+++ config/adminlte.php
@@ -124,7 +124,7 @@ return [
     'classes_body' => '',
     'classes_brand' => '',
     'classes_brand_text' => '',
-    'classes_content_wrapper' => '',
+    'classes_content_wrapper' => 'pt-3',
     'classes_content_header' => '',
     'classes_content' => '',
     'classes_sidebar' => 'sidebar-dark-primary elevation-4',
@@ -225,6 +225,66 @@ return [
     */
 
     'menu' => [
+        [
+            'type'         => 'navbar-search',
+            'text'         => 'search',
+            'topnav_right' => false,
+        ],
+        [
+            'type'         => 'fullscreen-widget',
+            'topnav_right' => false,
+        ],
+
+        // Sidebar items:
+        ['header' => 'Relação Comercial'],
+        [
+            'text' => 'Cliente',
+            'url'  => 'admin/relationship/customer',
+            'icon' => 'fas fa-user-tie',
+            'active' => ['admin/relationship/customer*']
+        ],
+        [
+            'text' => 'Fornecedor',
+            'url'  => 'admin/relationship/supplier',
+            'icon' => 'fas fa-industry',
+            'active' => ['admin/relationship/supplier*']
+        ],
+        ['header' => 'Cadastros'],
+        [
+            'text' => 'Conta Bancária',
+            'url'  => 'admin/bank',
+            'icon' => 'fas fa-university',
+            'active' => ['admin/bank*']
+        ],
+        [
+            'text' => 'Recorrências',
+            'url'  => 'admin/recurrence',
+            'icon' => 'fas fa-undo',
+            'active' => ['admin/recurrence*']
+        ],
+        ['header' => 'Movimentações'],
+        [
+            'text' => 'Conta a Receber',
+            'url'  => 'admin/charge/receive',
+            'icon' => 'fas fa-money-bill-alt',
+            'active' => ['admin/charge/receive*']
+        ],
+        [
+            'text' => 'Conta a Pagar',
+            'url'  => 'admin/charge/payment',
+            'icon' => 'fas fa-cash-register',
+            'active' => ['admin/charge/payment*']
+        ],
+        ['header' => 'Relatório'],
+        [
+            'text' => 'Pagamento',
+            'url'  => 'admin/payment',
+            'icon' => 'fas fa-money-bill-alt',
+            'active' => ['admin/payment*']
+        ],
+    ],
+
+    'menu2' => [
         // Navbar items:
         [
             'type'         => 'navbar-search',
@@ -401,12 +461,12 @@ return [
             ],
         ],
         'Sweetalert2' => [
-            'active' => false,
+            'active' => true,
             'files' => [
                 [
                     'type' => 'js',
-                    'asset' => false,
-                    'location' => '//cdn.jsdelivr.net/npm/sweetalert2@8',
+                    'asset' => true,
+                    'location' => 'js/sweetalert2@8.js',
                 ],
             ],
         ],
@@ -422,6 +482,21 @@ return [
                     'type' => 'js',
                     'asset' => false,
                     'location' => '//cdnjs.cloudflare.com/ajax/libs/pace/1.0.2/pace.min.js',
+                ],
+            ],
+        ],
+        'Admin' => [
+            'active' => true,
+            'files' => [
+                [
+                    'type' => 'css',
+                    'asset' => true,
+                    'location' => 'css/admin.css',
+                ],
+                [
+                    'type' => 'js',
+                    'asset' => true,
+                    'location' => 'js/admin.js',
                 ],
             ],
         ],
