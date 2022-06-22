f77d0006b4a1524c4580987b3f2e4d9722361610
Bruno Costa <bruno.costa@superlogica.com>
2022-06-22 17:08:33 -0300

    removendo

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,11 +2,8 @@
 
 namespace App\Console;
 
-use DateTime;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Log;
 
 class Kernel extends ConsoleKernel
 {
@@ -18,10 +15,10 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $date = (new DateTime())->format('Y-m-d');
-        $schedule->command("tenant:run payment:schedule --option='date={$date}'")->dailyAt('10:00:00');
-        $schedule->command("queue:work --stop-when-empty")->everyMinute();
-        $schedule->call(fn() => Http::get(config('app.url')))->everyTwoMinutes();
+        // $date = (new DateTime())->format('Y-m-d');
+        // $schedule->command("tenant:run payment:schedule --option='date={$date}'")->dailyAt('10:00:00');
+        // $schedule->command("queue:work --stop-when-empty")->everyMinute();
+        // $schedule->call(fn() => Http::get(config('app.url')))->everyTwoMinutes();
     }
 
     /**
