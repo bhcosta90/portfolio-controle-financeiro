ff80b67481e17c9ff8252bc1c448222107a9a574
Bruno Costa <bruno.costa@superlogica.com>
2022-06-25 11:06:00 -0300

    Teste de criacao, edição e exclusão das contas bancarias e recorrencia

--- tests/Unit/src/Financial/BankAccount/UseCases/DeleteUseCaseTest.php
+++ tests/Unit/src/Financial/BankAccount/UseCases/DeleteUseCaseTest.php
@@ -2,11 +2,15 @@
 
 namespace Tests\Unit\src\Financial\BankAccount\UseCases;
 
+use Core\Financial\Account\Domain\AccountEntity;
+use Core\Financial\Account\Repository\AccountRepositoryInterface;
 use PHPUnit\Framework\TestCase;
 use Core\Financial\BankAccount\Domain\BankAccountEntity as Entity;
 use Core\Financial\BankAccount\Repository\BankAccountRepositoryInterface as Repo;
 use Core\Financial\BankAccount\UseCases\DeleteUseCase;
+use Core\Shared\Interfaces\TransactionInterface;
 use Core\Shared\UseCases\Delete\{DeleteInput, DeleteOutput};
+use Core\Shared\ValueObjects\EntityObject;
 use Mockery;
 use Ramsey\Uuid\Uuid;
 
@@ -15,22 +19,117 @@ class DeleteUseCaseTest extends TestCase
     public function testHandleError()
     {
         $entity = Entity::create('bruno', 50, $id = Uuid::uuid4());
-
+        $account = AccountEntity::create(new EntityObject($entity->id(), $entity));
+        
         /** @var Repo|Mockery\MockInterface */
         $mock = Mockery::mock(stdClass::class, Repo::class);
         $mock->shouldReceive('find')->andReturn($entity);
         $mock->shouldReceive('delete')->andReturn(false);
+
+        /** @var AccountRepositoryInterface|Mockery\MockInterface */
+        $mockAccount = Mockery::mock(stdClass::class, AccountRepositoryInterface::class);
+        $mockAccount->shouldReceive('find')->andReturn($account);
+        $mockAccount->shouldReceive('delete')->andReturn(false);
+
+        /** @var TransactionInterface|Mockery\MockInterface */
+        $mockTransaction = Mockery::mock(stdClass::class, TransactionInterface::class);
+        $mockTransaction->shouldReceive('commit');
+        $mockTransaction->shouldReceive('rollback');
         
         /** @var UpdateInput|Mockery\MockInterface */
         $mockInput = $this->getMockClass(originalClassName: DeleteInput::class);
 
         $uc = new DeleteUseCase(
-            repo: $mock
+            repo: $mock,
+            account: $mockAccount,
+            transaction: $mockTransaction,
         );
 
         $handle = $uc->handle(new $mockInput($id));
         $mock->shouldHaveReceived('find')->times(1);
         $mock->shouldHaveReceived('delete')->times(1);
+
+        $mockAccount->shouldHaveReceived('find')->times(1);
+        $mockAccount->shouldHaveReceived('delete')->times(1);
+        $this->assertInstanceOf(DeleteOutput::class, $handle);
+        $this->assertFalse($handle->success);
+    }
+
+    public function testHandleErrorAccount()
+    {
+        $entity = Entity::create('bruno', 50, $id = Uuid::uuid4());
+        $account = AccountEntity::create(new EntityObject($entity->id(), $entity));
+        
+        /** @var Repo|Mockery\MockInterface */
+        $mock = Mockery::mock(stdClass::class, Repo::class);
+        $mock->shouldReceive('find')->andReturn($entity);
+        $mock->shouldReceive('delete')->andReturn(true);
+
+        /** @var AccountRepositoryInterface|Mockery\MockInterface */
+        $mockAccount = Mockery::mock(stdClass::class, AccountRepositoryInterface::class);
+        $mockAccount->shouldReceive('find')->andReturn($account);
+        $mockAccount->shouldReceive('delete')->andReturn(false);
+
+        /** @var TransactionInterface|Mockery\MockInterface */
+        $mockTransaction = Mockery::mock(stdClass::class, TransactionInterface::class);
+        $mockTransaction->shouldReceive('commit');
+        $mockTransaction->shouldReceive('rollback');
+        
+        /** @var UpdateInput|Mockery\MockInterface */
+        $mockInput = $this->getMockClass(originalClassName: DeleteInput::class);
+
+        $uc = new DeleteUseCase(
+            repo: $mock,
+            account: $mockAccount,
+            transaction: $mockTransaction,
+        );
+
+        $handle = $uc->handle(new $mockInput($id));
+        $mock->shouldHaveReceived('find')->times(1);
+        $mock->shouldHaveReceived('delete')->times(1);
+
+        $mockAccount->shouldHaveReceived('find')->times(1);
+        $mockAccount->shouldHaveReceived('delete')->times(1);
+        $this->assertInstanceOf(DeleteOutput::class, $handle);
+        $this->assertFalse($handle->success);
+    }
+
+    public function testHandleErrorBankAccount()
+    {
+        $entity = Entity::create('bruno', 50, $id = Uuid::uuid4());
+        $account = AccountEntity::create(new EntityObject($entity->id(), $entity));
+        
+        /** @var Repo|Mockery\MockInterface */
+        $mock = Mockery::mock(stdClass::class, Repo::class);
+        $mock->shouldReceive('find')->andReturn($entity);
+        $mock->shouldReceive('delete')->andReturn(false);
+
+        /** @var AccountRepositoryInterface|Mockery\MockInterface */
+        $mockAccount = Mockery::mock(stdClass::class, AccountRepositoryInterface::class);
+        $mockAccount->shouldReceive('find')->andReturn($account);
+        $mockAccount->shouldReceive('delete')->andReturn(true);
+
+        /** @var TransactionInterface|Mockery\MockInterface */
+        $mockTransaction = Mockery::mock(stdClass::class, TransactionInterface::class);
+        $mockTransaction->shouldReceive('commit');
+        $mockTransaction->shouldReceive('rollback');
+        
+        /** @var UpdateInput|Mockery\MockInterface */
+        $mockInput = $this->getMockClass(originalClassName: DeleteInput::class);
+
+        $uc = new DeleteUseCase(
+            repo: $mock,
+            account: $mockAccount,
+            transaction: $mockTransaction,
+        );
+
+        $handle = $uc->handle(new $mockInput($id));
+        $mock->shouldHaveReceived('find')->times(1);
+        $mock->shouldHaveReceived('delete')->times(1);
+
+        $mockAccount->shouldHaveReceived('find')->times(1);
+        $mockAccount->shouldHaveReceived('delete')->times(1);
+
         $this->assertInstanceOf(DeleteOutput::class, $handle);
         $this->assertFalse($handle->success);
     }
@@ -38,22 +137,38 @@ class DeleteUseCaseTest extends TestCase
     public function testHandle()
     {
         $entity = Entity::create('bruno', 50, $id = Uuid::uuid4());
-
+        $account = AccountEntity::create(new EntityObject($entity->id(), $entity));
+        
         /** @var Repo|Mockery\MockInterface */
         $mock = Mockery::mock(stdClass::class, Repo::class);
         $mock->shouldReceive('find')->andReturn($entity);
         $mock->shouldReceive('delete')->andReturn(true);
+
+        /** @var AccountRepositoryInterface|Mockery\MockInterface */
+        $mockAccount = Mockery::mock(stdClass::class, AccountRepositoryInterface::class);
+        $mockAccount->shouldReceive('find')->andReturn($account);
+        $mockAccount->shouldReceive('delete')->andReturn(true);
+
+        /** @var TransactionInterface|Mockery\MockInterface */
+        $mockTransaction = Mockery::mock(stdClass::class, TransactionInterface::class);
+        $mockTransaction->shouldReceive('commit');
+        $mockTransaction->shouldReceive('rollback');
         
         /** @var UpdateInput|Mockery\MockInterface */
         $mockInput = $this->getMockClass(originalClassName: DeleteInput::class);
 
         $uc = new DeleteUseCase(
-            repo: $mock
+            repo: $mock,
+            account: $mockAccount,
+            transaction: $mockTransaction,
         );
 
         $handle = $uc->handle(new $mockInput($id));
         $mock->shouldHaveReceived('find')->times(1);
         $mock->shouldHaveReceived('delete')->times(1);
+
+        $mockAccount->shouldHaveReceived('find')->times(1);
+        $mockAccount->shouldHaveReceived('delete')->times(1);
         $this->assertInstanceOf(DeleteOutput::class, $handle);
         $this->assertTrue($handle->success);
     }
