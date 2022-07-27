ac3dd630b3a8c77075982051f969c3c7cb146ef1
Bruno Costa <bruno.costa@superlogica.com>
2022-07-27 16:27:52 -0300

    finalizado

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('payment:execute')->everyTenMinutes();
+        $schedule->command('payment:execute')->daily('10:00:00');
     }
 
     /**
