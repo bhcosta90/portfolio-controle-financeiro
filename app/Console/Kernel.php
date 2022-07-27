d1415aec996e47deae4d952eb850490810c0a1ee
Bruno Costa <bruno.costa@superlogica.com>
2022-07-27 09:26:58 -0300

    mudando o horario

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('payment:execute')->hourly();
+        $schedule->command('payment:execute')->daily('07:00');
     }
 
     /**
