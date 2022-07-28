d878c96902870c745b32d77edf94eb5154b4d235
Bruno Costa <bruno.costa@superlogica.com>
2022-07-28 17:40:27 -0300

    add report

---
+++ app/Http/Controllers/Web/Admin/ReportController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Web;
+
+use App\Http\Controllers\Controller;
+use Core\Report\Services;
+use Illuminate\Http\Request;
+
+class ReportController extends Controller
+{
+    public function index(string $report, Request $request)
+    {
+        $letter = substr($report, -1);
+        $report = substr($report, 0, -1);
+        $reportClass = app('Core\\Application\\Report\\Reports\\R' . $report);
+
+        $objService = new Services\GenerateService($reportClass, $letter);
+        $ret = $objService->handle(new Services\DTO\Generate\Input(
+            $request->render ?? "html",
+            $request->all()
+        ));
+        $title = $ret->report['title'] ?: "";
+        $render = $ret->render();
+
+        return view('admin.report.index', compact('render', 'title'));
+    }
+}
---
+++ resources/views/admin/report/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class='card'>
+        <div class='card-header'>
+            <h1 class='float-left'>{{ __($title) }}</h1>
+            {{-- <div class='float-right'>{!! printOut(route('admin.payment.print', request()->all()), __('Imprimir')) !!}</div> --}}
+        </div>
+        {!! $render !!}
+    </div>
+@endsection
