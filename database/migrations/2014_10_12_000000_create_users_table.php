0e3c5d39d9e43ca97b42b42ab03dc03b2f38e286
Bruno Costa <bruno.costa@dafiti.com.br>
2023-09-11 17:25:40 -0300

    wip

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
             $table->foreignUuid('tenant_id')->on('tenants');
             $table->string('name');
             $table->string('email')->unique();
