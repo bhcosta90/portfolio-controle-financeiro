cf143d3950451ef532f16887540e168c1cc0aa50
Bruno Costa <bruno.costa@dafiti.com.br>
2023-09-11 17:24:03 -0300

    wip

--- config/tenants.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-return [
-    'central_domain' => explode(',', env('CENTRAL_DOMAIN')),
-];
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Tenant;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +14,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $tenant = Tenant::create([
+            'id' => 'a23ded4d-d0d8-4736-88ba-22aadc588f02',
+            'tenancy_db_name' => env('DB_DATABASE'),
+            'tenancy_db_host' => env('DB_HOST'),
+            'tenancy_db_username' => env('DB_USERNAME'),
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $tenant->domains()->create([
+            'domain' => 'demo'
+        ]);
+
+        tenancy()->initialize($tenant);
+
+        User::factory()->create([
+            'id' => '220ffc42-aa07-4f00-822c-f1cf8a2c0dd3',
+            'name' => 'Bruno Henrique da Costa',
+            'email' => 'bhcosta90@gmail.com',
+            'password' => '$2y$10$hoTymm/4dqKbYRCNWx7TKuAMOHmh/Som9xZvhpLuVMQmzcbHsg9OC'
+        ]);
     }
 }
