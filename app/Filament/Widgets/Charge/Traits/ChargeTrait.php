633bb926c7f16a686ed8c21be943ed02af6dc267
Bruno Costa <bruno.costa@dafiti.com.br>
2023-09-21 16:03:25 -0300

    wip

--- app/Filament/Widgets/Charge/Traits/ChargeTrait.php
+++ app/Filament/Widgets/Charge/Traits/ChargeTrait.php
@@ -5,25 +5,15 @@ namespace App\Filament\Widgets\Charge\Traits;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 trait ChargeTrait
 {
     public function last(Table $table, Resource $resource): Table
     {
-        $tableModel = $resource::getEloquentQuery()->getModel()->getTable();
-
         return $table
-            ->query(
-                $resource::getEloquentQuery()
-                    ->select("{$tableModel}.*")
-                    ->whereBetween('charges.due_date', [
-                        now()->firstOfMonth()->format('Y-m-d'),
-                        now()->lastOfMonth()->format('Y-m-d')
-                    ])
-                    ->whereNull('is_payed')
-                    ->join('charges', 'charges.charge_id', '=', "{$tableModel}.id")
-            )
+            ->query($this->query($resource))
             ->defaultPaginationPageOption(5)
             ->defaultSort('due_date', 'desc')
             ->columns([
@@ -42,5 +32,19 @@ trait ChargeTrait
                     ->label(__('filament-actions::edit.single.label'))
                     ->url(fn(Model $record): string => $resource::getUrl('edit', ['record' => $record])),
             ])->paginated(false);
+    }
+
+    public function query(Resource $resource): Builder
+    {
+        $tableModel = $resource::getEloquentQuery()->getModel()->getTable();
+
+        return $resource::getEloquentQuery()
+            ->select("{$tableModel}.*")
+            ->whereBetween('charges.due_date', [
+                now()->firstOfMonth()->format('Y-m-d'),
+                now()->lastOfMonth()->format('Y-m-d')
+            ])
+            ->whereNull('is_payed')
+            ->join('charges', 'charges.charge_id', '=', "{$tableModel}.id");
     }
 }
