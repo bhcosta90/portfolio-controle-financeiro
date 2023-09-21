061d3c432da481be6200dfc2f47d624bb6c296cd
Bruno Costa <bruno.costa@dafiti.com.br>
2023-09-21 10:35:33 -0300

    wip

--- app/Filament/OldWidgets/LastReceive.php
+++ app/Filament/OldWidgets/LastReceive.php
@@ -1,15 +1,15 @@
 <?php
 
-namespace App\Filament\Widgets;
+namespace App\Filament\OldWidgets;
 
-use App\Filament\Resources\Charge\PaymentResource;
-use App\Models\Charge\Payment;
+use App\Filament\Resources\Charge\ReceiveResource;
+use App\Models\Charge\Receive;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Filament\Widgets\TableWidget as BaseWidget;
 use Illuminate\Contracts\Support\Htmlable;
 
-class LastPayment extends BaseWidget
+class LastReceive extends BaseWidget
 {
     protected int|string|array $columnSpan = 'full';
 
@@ -17,16 +17,16 @@ class LastPayment extends BaseWidget
 
     protected function getTableHeading(): string|Htmlable|null
     {
-        return __('Despesas do mês');
+        return __('Receita do mês');
     }
 
     public function table(Table $table): Table
     {
-        $tableModel = PaymentResource::getEloquentQuery()->getModel()->getTable();
+        $tableModel = ReceiveResource::getEloquentQuery()->getModel()->getTable();
 
         return $table
             ->query(
-                PaymentResource::getEloquentQuery()
+                ReceiveResource::getEloquentQuery()
                     ->select("{$tableModel}.*")
                     ->whereBetween('charges.due_date', [
                         now()->firstOfMonth()->format('Y-m-d'),
@@ -50,7 +50,7 @@ class LastPayment extends BaseWidget
             ->actions([
                 Tables\Actions\Action::make('open')
                     ->label(__('filament-actions::edit.single.label'))
-                    ->url(fn (Payment $record): string => PaymentResource::getUrl('edit', ['record' => $record])),
+                    ->url(fn (Receive $record): string => ReceiveResource::getUrl('edit', ['record' => $record])),
             ]);
     }
 }
